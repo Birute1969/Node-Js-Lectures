0abf685a8f3f2e6f051333c5d63415fd8d9c5c8d
Birute1969 <107874553+Birute1969@users.noreply.github.com>
2022-12-15 21:34:31 +0200

    Added Texarea description

---
+++ paskaita-12-2/app/models/advert.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const advertSchema = new mongoose.Schema({
+    brand: {
+        type: String,
+        required: true
+    },
+    model: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    //pridedame description
+    description: {
+        type: String,
+        required: true
+    },
+    user_id: {
+        type: String,
+        required: true
+    }
+});
+const advertModel = mongoose.model('advert', advertSchema);
+
+module.exports = advertModel;
